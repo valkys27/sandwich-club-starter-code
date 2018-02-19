f2005aada534579f27910edbd1551869db0e9fe2
Tomáš Valko <st43355@student.upce.cz>
2018-02-19 11:19:55 +0100

    Add ButterKnife to inject views to Android components

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -11,17 +11,25 @@ import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
 
+import butterknife.*;
+
 public class DetailActivity extends AppCompatActivity {
 
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
 
+    @BindView(R.id.image_iv) ImageView ingredientsIv;
+    @BindView(R.id.also_known_tv) TextView alsoKnownAsTv;
+    @BindView(R.id.also_known_tv_label) TextView alsoKnownAsLabelTv;
+    @BindView(R.id.origin_tv) TextView placeOfOriginTv;
+    @BindView(R.id.ingredients_tv) TextView ingredientsTv;
+    @BindView(R.id.description_tv) TextView descriptionTv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
-
-        ImageView ingredientsIv = findViewById(R.id.image_iv);
+        ButterKnife.bind(this);
 
         Intent intent = getIntent();
         if (intent == null) {
@@ -60,19 +68,14 @@ public class DetailActivity extends AppCompatActivity {
     }
 
     private void populateUI(Sandwich sandwich) {
-        TextView alsoKnownAs = findViewById(R.id.also_known_tv);
         if (sandwich.getAlsoKnownAs().size() > 0) {
-            alsoKnownAs.setText(TextUtils.join(", ", sandwich.getAlsoKnownAs()));
+            alsoKnownAsTv.setText(TextUtils.join(", ", sandwich.getAlsoKnownAs()));
         } else {
-            TextView alsoKnownAsLabel = findViewById(R.id.also_known_tv_label);
-            alsoKnownAsLabel.setVisibility(View.GONE);
-            alsoKnownAs.setVisibility(View.GONE);
+            alsoKnownAsLabelTv.setVisibility(View.GONE);
+            alsoKnownAsTv.setVisibility(View.GONE);
         }
-        TextView placeOfOrigin = findViewById(R.id.origin_tv);
-        placeOfOrigin.setText(sandwich.getPlaceOfOrigin());
-        TextView ingredients = findViewById(R.id.ingredients_tv);
-        ingredients.setText(TextUtils.join(", ", sandwich.getIngredients()));
-        TextView description = findViewById(R.id.description_tv);
-        description.setText(sandwich.getDescription());
+        placeOfOriginTv.setText(sandwich.getPlaceOfOrigin());
+        ingredientsTv.setText(TextUtils.join(", ", sandwich.getIngredients()));
+        descriptionTv.setText(sandwich.getDescription());
     }
 }
